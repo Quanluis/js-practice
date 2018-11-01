813305653b8b3b0da3d40cd59752c3c2468913a5
Quanluis <quanluis@protonmail.com>
2018-11-01 16:04:14 -0400

    conditional if else statements

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("Hello World.")
+
+
+let x = 5; 
+
+let y = 10;
+
+console.log(x);
+console.log(y);
+
+if  (x > y) {
+    console.log("X is greater than Y.")
+}else{
+    console.log("X is not not greater than Y.")
+}
+
+
+
